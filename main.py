dc460231daf0c6c422a1bf2dd9a115762b5e5472
ps160202 <pratham160202@gmail.com>
2022-07-27 19:04:35 +0530

    2 Player game with temporary popup window

--- main.py
+++ main.py
@@ -3,134 +3,242 @@ from tkinter import ttk
 from tkinter import font as tkFont
 
 turn = 1
+boxes = 0
 
-def checkWinner():
+def checkWinner(box):
+    str = ""
+    player = ""
+    winner = 0
+
+    if turn == 1:
+        player = 'O'
+        winner = 2
+    elif turn == 2:
+        player = 'X'
+        winner = 1
+
+
+    if box == 1:
+        if((box2['text'] == player and box3['text'] == player) or
+                (box4['text'] == player and box7['text'] == player) or
+                (box5['text'] == player and box9['text'] == player)):
+            str = "Player %s wins!" % winner
+    elif box == 2:
+        if ((box5['text'] == player and box8['text'] == player) or
+                (box1['text'] == 'O' and box3['text'] == player)):
+            str = "Player %s wins!" % winner
+    elif box == 3:
+        if ((box1['text'] == player and box2['text'] == player) or
+                (box5['text'] == player and box7['text'] == player) or
+                (box6['text'] == player and box9['text'] == player)):
+            str = "Player %s wins!" % winner
+    elif box == 4:
+        if ((box1['text'] == player and box7['text'] == player) or
+                (box5['text'] == player and box9['text'] == player)):
+            str = "Player %s wins!" % winner
+    elif box == 5:
+        if ((box1['text'] == player and box9['text'] == player) or
+                (box3['text'] == player and box7['text'] == player) or
+                (box4['text'] == player and box6['text'] == player) or
+                (box2['text'] == player and box8['text'] == player)):
+            str = "Player %s wins!" % winner
+    elif box == 6:
+        if ((box3['text'] == player and box9['text'] == player) or
+                (box4['text'] == player and box5['text'] == player)):
+            str = "Player %s wins!" % winner
+    elif box == 7:
+        if ((box1['text'] == player and box4['text'] == player) or
+                (box5['text'] == player and box3['text'] == player) or
+                (box8['text'] == player and box9['text'] == player)):
+            str = "Player %s wins!" % winner
+    elif box == 8:
+        if ((box7['text'] == player and box9['text'] == player) or
+                (box2['text'] == player and box5['text'] == player)):
+            str = "Player %s wins!" % winner
+    elif box == 9:
+        if ((box7['text'] == player and box8['text'] == player) or
+                (box1['text'] == player and box5['text'] == player) or
+                (box3['text'] == player and box6['text'] == player)):
+            str = "Player %s wins!" % winner
+
+    if str != "":
+        top = Toplevel(tk)
+        top.geometry("750x250")
+        top.title("Child Window")
+        Label(top, text=str).place(x=150, y=80)
+
+    if boxes == 9 and str != "":
+        top = Toplevel(tk)
+        top.geometry("750x250")
+        top.title("Child Window")
+        Label(top, text="DRAW!").place(x=150, y=80)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     return
 
+
 def buttonClick1():
-    global turn
+    global turn, boxes
     if(box1['text'] == 'X' or box1['text'] == '0'):
         return
     elif(turn == 1):
         box1['text'] = 'X'
         turn = 2
+        boxes += 1
     elif(turn == 2):
         box1['text'] = 'O'
         turn = 1
+        boxes += 1
 
-    checkWinner()
+    checkWinner(1)
     return
 
+
 def buttonClick2():
-    global turn
+    global turn, boxes
     if(box2['text'] == 'X' or box2['text'] == '0'):
         return
     elif(turn == 1):
         box2['text'] = 'X'
         turn = 2
+        boxes += 1
     elif(turn == 2):
         box2['text'] = 'O'
         turn = 1
+        boxes += 1
 
-    checkWinner()
+    checkWinner(2)
     return
 
+
 def buttonClick3():
-    global turn
+    global turn, boxes
     if(box3['text'] == 'X' or box3['text'] == '0'):
         return
     elif(turn == 1):
         box3['text'] = 'X'
         turn = 2
+        boxes += 1
     elif(turn == 2):
         box3['text'] = 'O'
         turn = 1
+        boxes += 1
 
-    checkWinner()
+    checkWinner(3)
     return
 
+
 def buttonClick4():
-    global turn
+    global turn, boxes
     if(box4['text'] == 'X' or box4['text'] == '0'):
         return
     elif(turn == 1):
         box4['text'] = 'X'
         turn = 2
+        boxes += 1
     elif(turn == 2):
         box4['text'] = 'O'
         turn = 1
+        boxes += 1
 
-    checkWinner()
+    checkWinner(4)
     return
 
+
 def buttonClick5():
-    global turn
+    global turn, boxes
     if(box5['text'] == 'X' or box5['text'] == '0'):
         return
     elif(turn == 1):
         box5['text'] = 'X'
         turn = 2
+        boxes += 1
     elif(turn == 2):
         box5['text'] = 'O'
         turn = 1
+        boxes += 1
 
-    checkWinner()
+    checkWinner(5)
     return
 
+
 def buttonClick6():
-    global turn
+    global turn, boxes
     if(box6['text'] == 'X' or box6['text'] == '0'):
         return
     elif(turn == 1):
         box6['text'] = 'X'
         turn = 2
+        boxes += 1
     elif(turn == 2):
         box6['text'] = 'O'
         turn = 1
+        boxes += 1
 
-    checkWinner()
+    checkWinner(6)
     return
 
+
 def buttonClick7():
-    global turn
+    global turn, boxes
     if(box7['text'] == 'X' or box7['text'] == '0'):
         return
     elif(turn == 1):
         box7['text'] = 'X'
         turn = 2
+        boxes += 1
     elif(turn == 2):
         box7['text'] = 'O'
         turn = 1
+        boxes += 1
 
-    checkWinner()
+    checkWinner(7)
     return
 
+
 def buttonClick8():
-    global turn
+    global turn, boxes
     if(box8['text'] == 'X' or box8['text'] == '0'):
         return
     elif(turn == 1):
         box8['text'] = 'X'
         turn = 2
+        boxes
     elif(turn == 2):
         box8['text'] = 'O'
         turn = 1
+        boxes
 
-    checkWinner()
+    checkWinner(8)
     return
 
+
 def buttonClick9():
-    global turn
+    global turn, boxes
     if(box9['text'] == 'X' or box9['text'] == '0'):
         return
     elif(turn == 1):
         box9['text'] = 'X'
         turn = 2
+        boxes += 1
     elif(turn == 2):
         box9['text'] = 'O'
         turn = 1
+        boxes += 1
 
-    checkWinner()
+    checkWinner(9)
     return
 
 
@@ -193,7 +301,7 @@ frame7.grid(row=2, column=0)
 frame7.grid_propagate(False) #disables resizing of frame
 frame7.columnconfigure(0, weight=1) #enables button to fill frame
 frame7.rowconfigure(0,weight=1)
-box7 = ttk.Button(frame7, text="-", command=buttonClick7)
+box7 = ttk.Button(frame7, text="", command=buttonClick7)
 box7.grid(sticky="wens")
 
 frame8 = ttk.Frame(tk, width=100, height=100)
